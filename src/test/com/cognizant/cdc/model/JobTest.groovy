84fd35a52c674de46390a659c4ec5bbe90f85a85
Jeff <realdah@gmail.com>
2014-12-08 17:13:34 +0800

    Update about.txt

--- src/test/com/cognizant/cdc/model/JobTest.groovy
+++ src/test/com/cognizant/cdc/model/JobTest.groovy
@@ -12,12 +12,13 @@ class JobTest {
 
     @Test
     void test() {
-        List<Term> parse = IndexAnalysis.parse("大龄女青年");
-        for(Term term : parse) {
-            String nature = term.getNatureStr()
-//            if(nature != "null" && ( nature == "en" || nature.startsWith("n") || nature.startsWith("v") || nature.startsWith("a") )) {
-                println term.getName() + "  " + term.getNatureStr()
-//            }
-        }
+//        List<Term> parse = IndexAnalysis.parse("大龄女青年");
+//        for(Term term : parse) {
+//            String nature = term.getNatureStr()
+////            if(nature != "null" && ( nature == "en" || nature.startsWith("n") || nature.startsWith("v") || nature.startsWith("a") )) {
+//                println term.getName() + "  " + term.getNatureStr()
+////            }
+//        }
+
     }
 }
